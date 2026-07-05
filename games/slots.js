9ef8fc5bcafcae2a53d825aa8c91af13958cad4f
LameLeo <leonhardmuehlbacher123@gmail.com>
2026-07-05 11:43:59 +0200

    Fix audio file paths for slot sounds

--- games/slots.js
+++ games/slots.js
@@ -19,10 +19,10 @@ const symbols = [
 ];
 
 let slotRunning = false;
-const spinSound = new Audio("..sounds/spin.mp3.wav");
-const winSound = new Audio("..sounds/win.mp3.wav");
-const loseSound = new Audio("..sounds/lose.mp3.wav");
-const jackpotSound = new Audio("..sounds/jackpot.mp3.wav");
+const spinSound = new Audio("../sounds/spin.mp3.wav");
+const winSound = new Audio("../sounds/win.mp3.wav");
+const loseSound = new Audio("../sounds/lose.mp3.wav");
+const jackpotSound = new Audio("../sounds/jackpot.mp3.wav");
 
 spinSound.volume = 0.4;
 winSound.volume = 0.5;
